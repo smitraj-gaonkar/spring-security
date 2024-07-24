2ac2f2eb70910e3b6fa776f485439091dc3b6b50
smitrajgaonkar7@gmail.com <smitrajgaonkar7@gmail.com>
2024-07-24 22:52:07 +0530

    v3: Spring Security v5 with In-Memory UserDetailsManager

--- src/main/java/com/example/spring_security/SpringSecurityApplication.java
+++ src/main/java/com/example/spring_security/SpringSecurityApplication.java
@@ -2,7 +2,7 @@ package com.example.spring_security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+// import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 // @SpringBootApplication(exclude = { SecurityAutoConfiguration.class }) //To disable spring security
--- src/main/java/com/example/spring_security/configuration/security/SecurityConfig.java
+++ src/main/java/com/example/spring_security/configuration/security/SecurityConfig.java
@@ -1,28 +1,64 @@
 package com.example.spring_security.configuration.security;
 
-
 /* Spring Security v4.x.x to v5.6.0 */
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
-// @Configuration
-// @EnableWebSecurity
-// public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//     @Override
-//     protected void configure(HttpSecurity http) throws Exception {
-//         http
-//                 .csrf(csrf -> csrf.disable())
-//                 .authorizeRequests(requests -> requests
-//                         .antMatchers("/", "/public/**").permitAll()
-//                         .anyRequest().authenticated())
-//                 .formLogin(withDefaults());
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(requests -> requests
+                        .antMatchers("/", "/public/**").permitAll()
+                        .antMatchers("/private/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .sessionManagement(management -> management
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // .formLogin(withDefaults())  // Use Form-based Authentication
+                .httpBasic(withDefaults())  // Use Basic Authentication
+                ;
             
-//     }
-// }
+    }
 
-public class SecurityConfig {}
+    @Bean
+    public UserDetailsService users() {
+
+        UserDetails admin = User.builder()
+            .username("admin")
+            .password(passwordEncoder().encode("pass"))
+            .roles("ADMIN")
+            .build();
+        
+        UserDetails user = User.builder()
+            .username("user")
+            .password(passwordEncoder().encode("pass"))
+            .roles("USER")
+            .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
+// public class SecurityConfig {}
 
